bdbc0fb51216d5332258d01d17b47059f2858017
Vuyisile Ndlovu <terrameijar@gmail.com>
2017-03-14 01:13:20 +0200

    This script uses requests.json()

---
+++ ie_6_visits.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# The number of people who visited a U.S government 
+# website using Internet Explorer 6.0 in the last 90 days
+
+import requests
+
+
+r = requests.get("https://analytics.usa.gov/data/live/ie.json")
+print(r.json()['totals']['ie_version']['6.0'])
