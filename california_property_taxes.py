82e5b3331887f9f5d22ae831bd3bee4b42896f59
Vuyisile Ndlovu <terrameijar@gmail.com>
2017-03-21 10:45:58 +0200

    Script can download and unpack dataset

--- california_property_taxes.py
+++ california_property_taxes.py
@@ -1,8 +1,10 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 import os.path
 import unittest
 import requests
 from bs4 import BeautifulSoup
+from shutil import unpack_archive
+
 
 # How much did the state of California collect in property taxes, 
 # according to the U.S. Census 2013 Annual Survey of State 
@@ -13,6 +15,7 @@ link = 'https://www.census.gov/govs/statetax/historical_data.html'
 filename = 'STC_Historical_DB.xls'
 dataset_zipfile = None
 temp_dir = '/tmp'
+local_data_dir = '/tmp/state_tax_collections/' # For extracted data
 #data = 'http://www2.census.gov/govs/statetax/stcfy16.zip'
 
 # TODO: 1) Get the dataset from url
@@ -21,19 +24,20 @@ soup = BeautifulSoup(response.text, 'lxml')
 # URL of the dataset zip file
 data = soup.select('div.hi-lite ul li a')[0]['href']
 base_filename = os.path.basename(data)
-#print(base_filename)  # stcfy16.zip
+# stcfy16.zip
 
 # TODO: 2) Save the dataset to a temp folder
-
 local_zipname = os.path.join(temp_dir, base_filename)
-# we will cache the download because of it's sheer size
+# we will cache the download
 if not os.path.exists(local_zipname):
     print('Downloading dataset from %s to %s' % (link, local_zipname))
     zipfile_data = requests.get(data).content
     with open(local_zipname, 'wb') as f:
         f.write(zipfile_data)
 
-# TODO: 3) Unpack the dataset
+# TODO: Downloaded, now unpack the dataset
+print('Unzipping %s to %s' % (local_zipname, local_data_dir))
+unpack_archive(local_zipname, local_data_dir, format = 'zip')
 # TODO: 4) Open the statistics dataset
 # TODO: 5) Get property tax data for State of California
 # TODO: **Write the using TDD**
